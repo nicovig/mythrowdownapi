7cd7bb496e206a942d3db77c4450bf936744c58b
Nicolas Vigouroux <nvigouroux@eig.fr>
2023-04-05 17:23:04 +0200

    get route

--- app.js
+++ app.js
@@ -2,23 +2,26 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('requête reçue');
-    next();
-});
-
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
-
-app.use((req, res, next) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
-   next();
-});
-
-app.use((req, res, next) => {
-    console.log('requête reçue');
- });
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
 
 module.exports = app;
